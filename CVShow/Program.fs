8bd666574776f27eed087282261b0b565583b82d
June <naiveddd@gmail.com>
2021-12-02 12:39:40 +0800

    Basic Image show.

---
+++ CVShow/Program.fs
@@ -0,0 +1,10 @@
+﻿open SharpCV
+let cv2 = SharpCV.Binding.cv2
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let img = cv2.imread("../../../../../18_2703.jpg")
+cv2.imshow("Manga", img)
+
+cv2.waitKey(0)

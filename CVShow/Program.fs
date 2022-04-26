197c1792a87773e2ca6674a2780a76ec5fabaab7
NAIVEddd <naiveddd@gmail.com>
2022-04-26 17:19:47 +0800

    Assignment4 completed.

--- CVShow/Program.fs
+++ CVShow/Program.fs
@@ -6,6 +6,7 @@ open RayTracing2
 open IFS_6_837
 open Assignment1_6_837
 open Assignment2_6_837
+open Assignment4_6_837
 
 // For more information see https://aka.ms/fsharp-console-apps
 //let model = LoadModel("C:\\allFiles\\Game\\LittleGraphicShow\\.vs\\Assets\\Renault12TL\\Renault12TL.obj")
@@ -19,4 +20,5 @@ open Assignment2_6_837
 
 //IFS()
 //LoadScene()
-LoadScene_Assignment2()
+//LoadScene_Assignment2()
+LoadScene_Assignment4()
